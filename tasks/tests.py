b23e6e6a6a010905de4e1936c0ee8cb1b3471ffb
budhraja.ankita <budhraja.ankita@gmail.com>
2024-04-22 02:30:58 -0700

    failed test

--- tasks/tests.py
+++ tasks/tests.py
@@ -6,4 +6,4 @@ class TaskModelTest(TestCase):
         task = Task.objects.create(title="Sample Task", description="Sample Description", completed=False)
         self.assertEqual(task.title, "Sample Task")
         self.assertEqual(task.description, "Sample Description")
-        self.assertFalse(task.completed)
+        self.assertTrue(task.completed)
