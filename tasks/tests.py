a98eb1900bf5c38e8f3f992e1ee0dec2a68959e3
budhraja.ankita <budhraja.ankita@gmail.com>
2024-04-22 01:56:23 -0700

    add html and other files

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,16 @@
+from django.urls import reverse
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+class TaskTests(TestCase):
+
+    def test_task_creation(self):
+        task = Task.objects.create(
+            title='Test Task',
+            description='Test Description',
+            priority=2,
+        )
+        self.assertEqual(task.title, 'Test Task')
+        self.assertEqual(task.description, 'Test Description')
+        self.assertEqual(task.priority, 2)
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = Task.objects.get(id=pk)
+    return render(request, 'tasks/task_detail.html', {'task': task})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_update(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('task_list')
+    return render(request, 'tasks/task_form.html', {'form': form, 'task': task})
+
+def task_delete(request, pk):
+    Task.objects.get(id=pk).delete()
+    return redirect('task_list')
